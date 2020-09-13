b98ad4887fb40a6938fb174530d891bc09fafe70
S. Jacob Powell <s.jacob.powell@gmail.com>
2020-09-13 01:53:23 -0600

    Nice little polished calculator

--- src/App.js
+++ src/App.js
@@ -58,45 +58,62 @@ function App() {
       setyc('-')
       return
     }
-    const newm = b - (b * f)
-    const newbu = p * b
-    const newbbf = b - newm
-    const newbuf = p * newbbf
-    const newn = p + i
+
+    const newb = parseFloat(b)
+    const newp = parseFloat(p)
+    const newi = parseFloat(i)
+    const neww = parseFloat(w)
+
+    const newm = newb - (newb * f)
+    const newbu = newp * newb
+    const newbbf = newb - newm
+    const newbuf = newp * newbbf
+    const newn = newp + newi
     const news = newm * newn
     const newsbf = newm * f
     const newsb = newm - newsbf
     const newsuf = news * f
-    const newt = news - newsbf
-    const newd = newn / p
+    const newt = news - newsuf
+    const newd = newn / newp
     const newc = (newd - 1.0) * 100
 
-    const newr = b * i - (2.0 * b * p * f) - (2.0 * b * i * f) + (b * p * f * f) + (b * i * f * f)
+    const newr =
+      newb * newi -
+      (2.0 * newb * newp * f) -
+      (2.0 * newb * newi * f) +
+      (newb * newp * f * f) +
+      (newb * newi * f * f)
 
-    const newyb = w / (i - (2.0 * p * f) - (2.0 * i * f) + (p * f * f) + (i * f * f))
-    const newyi = (w + (2.0 * b * p * f) - (b * p * f * f)) / (b - (2.0 * b * f) + (b * f * f))
-    const newyd = (p + newyi) / p
+    const newyb =
+      neww / (newi - (2.0 * newp * f) -
+      (2.0 * newi * f) +
+      (newp * f * f) +
+      (newi * f * f))
+    const newyi =
+      (neww + (2.0 * newb * newp * f) - (newb * newp * f * f)) /
+      (newb - (2.0 * newb * f) + (newb * f * f))
+    const newyd = (newp + newyi) / newp
     const newyc = (newyd - 1.0) * 100
 
-    setm(newm.toFixed(8))
-    setbu(newbu.toFixed(2))
-    setbbf(newbbf.toFixed(8))
-    setbuf(newbuf.toFixed(2))
-    setn(newn.toFixed(2))
-    sets(news.toFixed(2))
-    sett(newt.toFixed(2))
-    setsb(newsb.toFixed(8))
-    setsbf(newsbf.toFixed(8))
-    setsuf(newsuf.toFixed(2))
+    setm(newm.toFixed(8) + " BTC")
+    setbu("$" + newbu.toFixed(2))
+    setbbf(newbbf.toFixed(8) + " BTC")
+    setbuf("$" + newbuf.toFixed(2))
+    setn("$" + newn.toFixed(2))
+    sets("$" + news.toFixed(2))
+    sett("$" + newt.toFixed(2))
+    setsb(newsb.toFixed(8) + " BTC")
+    setsbf(newsbf.toFixed(8) + " BTC")
+    setsuf("$" + newsuf.toFixed(2))
     setd(newd.toFixed(8))
-    setc(newc.toFixed(2))
+    setc(newc.toFixed(2) + "%")
 
-    setr(newr.toFixed(2))
+    setr("$" + newr.toFixed(2))
 
-    setyb(newyb.toFixed(8))
-    setyi(newyi.toFixed(2))
+    setyb(newyb.toFixed(8) + " BTC")
+    setyi("$" + newyi.toFixed(2))
     setyd(newyd.toFixed(8))
-    setyc(newyc.toFixed(2))
+    setyc(newyc.toFixed(2) + "%")
   }
 
   React.useEffect(calculate, [p, b, i, w])
@@ -115,42 +132,82 @@ function App() {
   return (
     <div className="App">
       <h1 className="Title">Trade Calculator</h1>
-
-      <div className="InputComponent">
-        <h2 className="InputLabel">Price (p)</h2>
-        <input className="Input" onChange={(e) => setp(e.target.value)} value={p}/>
+      <div className="Content">
+        <div className="Left">
+          <div className="Top">
+            <div className="Cell">
+              <div className="InputComponent">
+                <h2 className="InputLabel">Price (p)</h2>
+                <input className="Input" onChange={(e) => setp(e.target.value)} value={p}/>
+              </div>
+              <div className="InputComponent">
+                <h2 className="InputLabel">Buy (b)</h2>
+                <input className="Input" onChange={(e) => setb(e.target.value)} value={b}/>
+              </div>
+            </div>
+            <div className="Cell">
+              <ValueComponent label="Receive (m)" value={m}/>
+              <ValueComponent label="Receive (bu)" value={bu}/>
+            </div>
+            <div className="Cell">
+              <ValueComponent label="Fee (bbf)" value={bbf}/>
+              <ValueComponent label="Fee (buf)" value={buf}/>
+            </div>
+          </div>
+          <div className="Middle">
+            <div className="Cell">
+              <div className="InputComponent">
+                <h2 className="InputLabel">Increase (i)</h2>
+                <input className="Input" onChange={(e) => seti(e.target.value)} value={i}/>
+              </div>
+            </div>
+            <div className="Cell">
+              <ValueComponent label="Change (c)" value={c}/>
+              <ValueComponent label="Multiplier (d)" value={d}/>
+            </div>
+          </div>
+          <div className="Bottom">
+            <div className="Cell">
+              <ValueComponent label="New Price (n)" value={n}/>
+              <ValueComponent label="Sell (s)" value={s}/>
+            </div>
+            <div className="Cell">
+              <ValueComponent label="Receive (t)" value={t}/>
+              <ValueComponent label="Receive (sb)" value={sb}/>
+            </div>
+            <div className="Cell">
+              <ValueComponent label="Fee (suf)" value={suf}/>
+              <ValueComponent label="Fee (sbf)" value={sbf}/>
+            </div>
+          </div>
+        </div>
+        <div className="Right">
+          <div className="Top">
+            <div className="Cell">
+              <ValueComponent label="Fee (f)" value={f}/>
+              <div className="InputComponent">
+                <h2 className="InputLabel">Wanted Return (w)</h2>
+                <input className="Input" onChange={(e) => setw(e.target.value)} value={w}/>
+              </div>
+              <ValueComponent label="Needed b (yb)" value={yb}/>
+            </div>
+          </div>
+          <div className="Middle">
+            <div className="Cell">
+              <ValueComponent label="Needed i (yi)" value={yi}/>
+            </div>
+            <div className="Cell">
+              <ValueComponent label="Needed change (yc)" value={yc}/>
+              <ValueComponent label="Needed multiplier (yd)" value={yd}/>
+            </div>
+          </div>
+          <div className="Bottom">
+            <div className="Cell">
+              <ValueComponent label="Return (r)" value={r}/>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="InputComponent">
-        <h2 className="InputLabel">Buy BTC (b)</h2>
-        <input className="Input" onChange={(e) => setb(e.target.value)} value={b}/>
-      </div>
-      <div className="InputComponent">
-        <h2 className="InputLabel">Increase (i)</h2>
-        <input className="Input" onChange={(e) => seti(e.target.value)} value={i}/>
-      </div>
-      <div className="InputComponent">
-        <h2 className="InputLabel">Wanted Return (w)</h2>
-        <input className="Input" onChange={(e) => setw(e.target.value)} value={w}/>
-      </div>
-
-      <ValueComponent label="Fee (f)" value={f}/>
-      <ValueComponent label="Buy BTC AF (m)" value={m}/>
-      <ValueComponent label="USD (bu)" value={bu}/>
-      <ValueComponent label="BTC Buy Fee (bbf)" value={bbf}/>
-      <ValueComponent label="USD Buy Fee (buf)" value={buf}/>
-      <ValueComponent label="New Price (n)" value={n}/>
-      <ValueComponent label="Sell USD (s)" value={s}/>
-      <ValueComponent label="USD AF (t)" value={t}/>
-      <ValueComponent label="Sell BTC AF (sb)" value={sb}/>
-      <ValueComponent label="Sell BTC Fee (sbf)" value={sbf}/>
-      <ValueComponent label="Sell USD Fee (suf)" value={suf}/>
-      <ValueComponent label="Percent Change (c)" value={c}/>
-      <ValueComponent label="Multiplier (d)" value={d}/>
-      <ValueComponent label="B to make W (yb)" value={yb}/>
-      <ValueComponent label="I from B to make W (yi)" value={yi}/>
-      <ValueComponent label="Return (r)" value={r}/>
-      <ValueComponent label="C from B to make W (yc)" value={yc}/>
-      <ValueComponent label="D from B to make W (yd)" value={yd}/>
     </div>
   );
 }
